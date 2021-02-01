919360a9a6958206809ad18b79dc62f9fed9bf38
Jacob Marthaller <https://github.com/jmarthaller>
2021-02-01 17:58:14 -0500

    commit tree 4a0c13a255c717fb6e17725ca8228537daa933a7

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log(`before setState: ${count}`);
     setCount(count + 1);
+    console.log(`after setState once: ${count}`);
+    setCount(count + 1);
+    console.log(`after setState twice: ${count}`);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
